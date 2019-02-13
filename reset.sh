b835dcb4d2d60c2c34ebeac760977261e4f679de
facugon85 <40042139+facugon85@users.noreply.github.com>
2019-02-13 10:05:57 -0300

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(echo open 192.168.1.1; sleep 1; echo "Boxeway2018!"; sleep 3; 
+(echo open 192.168.1.1; sleep 1; echo "Contraseña del router"; sleep 3; 
 echo "dev reboot"; sleep 10;) | telnet
 
 #Reseteo via Telnet 
